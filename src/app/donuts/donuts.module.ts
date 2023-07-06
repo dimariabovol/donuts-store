950ce614a845366089247752ebd0e6b9d2f9034c
driabovol <driabovol@MacBook-Air-Dima.local>
2023-07-06 11:46:13 +0300

    Add footer component

--- src/app/donuts/donuts.module.ts
+++ src/app/donuts/donuts.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { DonutsRoutingModule } from './donuts-routing.module';
 import { DonutsComponent } from './donuts.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [DonutsComponent, HeaderComponent],
+  declarations: [DonutsComponent, FooterComponent, HeaderComponent],
   imports: [BrowserModule, DonutsRoutingModule],
   providers: [],
   bootstrap: [DonutsComponent],
