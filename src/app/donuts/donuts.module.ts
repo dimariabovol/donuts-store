5ea9efb32139516ea2fd09410e9b13f0c1256c2d
driabovol <driabovol@MacBook-Air-Dima.local>
2023-07-06 11:41:11 +0300

    Add header component

--- src/app/donuts/donuts.module.ts
+++ src/app/donuts/donuts.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { DonutsRoutingModule } from './donuts-routing.module';
 import { DonutsComponent } from './donuts.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [DonutsComponent],
+  declarations: [DonutsComponent, HeaderComponent],
   imports: [BrowserModule, DonutsRoutingModule],
   providers: [],
   bootstrap: [DonutsComponent],
