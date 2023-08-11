d085bbbab2ca0d6a97f4344f4f04a29d1e2587ba
driabovol <ryabovoldima@gmail.com>
2023-08-11 13:03:38 +0300

    Add header shadow when page scrolling

--- src/app/donuts/components/header/header.component.ts
+++ src/app/donuts/components/header/header.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 import { CartService } from 'src/app/cart/cart.service';
 
 @Component({
@@ -11,9 +18,18 @@ import { CartService } from 'src/app/cart/cart.service';
   },
 })
 export class HeaderComponent implements OnInit {
+  @HostBinding('class.header--shadowed') isShadowed: boolean = false;
+
+  @HostListener('window:scroll') onScroll() {
+    this.isShadowed = window.scrollY > 0;
+  }
+
   cartItemsCount: number = 0;
 
-  constructor(private _cartService: CartService) {}
+  constructor(
+    private _cartService: CartService,
+    private _viewportScroller: ViewportScroller,
+  ) {}
 
   ngOnInit() {
     this._cartService.cart$.subscribe(products => {
